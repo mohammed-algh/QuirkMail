5f47fed81c36b7d93fc2e4d538b31b031922712d
Mohammed Alghamdi <mohammedhalgh@hotmail.com>
2024-01-22 22:04:47 +0300

    Refactor SQLiteModule class to improve code readability and maintainability

--- database/sqlite_module.py
+++ database/sqlite_module.py
@@ -6,30 +6,41 @@ class SQLiteModule:
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
 
-    def __del__(self):
-        self.conn.close()
-
     def create_table(self, table_name, columns):
-        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS {} ({})".format(table_name, columns))
         self.conn.commit()
 
     def insert(self, table_name, columns, values):
-        self.cursor.execute(f"INSERT INTO {table_name} ({columns}) VALUES ({values})")
+        query = "INSERT INTO {} ({}) VALUES ({})".format(table_name, columns, values)
+        self.cursor.execute(query)
         self.conn.commit()
 
-    def select(self, table_name, columns, condition):
-
-        if condition == "":
-            self.cursor.execute(f"SELECT {columns} FROM {table_name}")
+    def select(self, table_name, columns, where=None):
+        if where:
+            query = "SELECT {} FROM {} WHERE {}".format(columns, table_name, where)
         else:
-            self.cursor.execute(f"SELECT {columns} FROM {table_name} WHERE {condition}")
-
+            query = "SELECT {} FROM {}".format(columns, table_name)
+        self.cursor.execute(query)
         return self.cursor.fetchall()
     
-    def update(self, table_name, columns, condition):
-        self.cursor.execute(f"UPDATE {table_name} SET {columns} WHERE {condition}")
+    def update(self, table_name, columns, where=None):
+        if where:
+            query = "UPDATE {} SET {} WHERE {}".format(table_name, columns, where)
+        else:
+            query = "UPDATE {} SET {}".format(table_name, columns)
+        self.cursor.execute(query)
         self.conn.commit()
 
-    def delete(self, table_name, condition):
-        self.cursor.execute(f"DELETE FROM {table_name} WHERE {condition}")
+    def delete(self, table_name, where=None):
+        if where:
+            query = "DELETE FROM {} WHERE {}".format(table_name, where)
+        else:
+            query = "DELETE FROM {}".format(table_name)
+        self.cursor.execute(query)
         self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
+    
+    def close(self):
+        self.conn.close()
